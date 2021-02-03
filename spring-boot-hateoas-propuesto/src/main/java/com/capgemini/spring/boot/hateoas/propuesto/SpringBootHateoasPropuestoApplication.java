2e9ac6d6797e025af9d96a245d88f33659a1cbb0
Álvaro Maestre <alvaro.maestre-santa@capgemini.com>
2021-02-03 17:59:16 +0100

    Creación proyecto Hateoas propuesto

---
+++ spring-boot-hateoas-propuesto/src/main/java/com/capgemini/spring/boot/hateoas/propuesto/SpringBootHateoasPropuestoApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.spring.boot.hateoas.propuesto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHateoasPropuestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHateoasPropuestoApplication.class, args);
+	}
+
+}
